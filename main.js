c2c99e21e92c493b5c6cb2d9f1769ae0347679a1
Thiago Sanches <thiagomlkgame@gmail.com>
2025-04-02 17:17:32 -0300

    Documentação da Atividade

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ const template = [
         submenu: [
             {
                 label: 'Clientes',
-                click: () => relatiorioClientes()
+                click: () => relatorioClientes()
             },
             {
                 label: 'OS abertas'
@@ -293,36 +293,85 @@ ipcMain.on('new-client', async (event, client) => {
 // ============================================================
 
 
-// ========Relatorios de clientes==============================
-async function relatiorioClientes() {
+// ============================================================
+// == Relatório de clientes ===================================
+
+async function relatorioClientes() {
     try {
-        // consultar o banco de daods e obter a listagem de clientes cadastrados por ordem alfabética
+        // Passo 1: Consultar o banco de dados e obter a listagem de clientes cadastrados por ordem alfabética
         const clientes = await clientModel.find().sort({ nomeCliente: 1 })
-        // ´- portrait | l - landscape | mm e a4 (folha)
+        // teste de recebimento da listagem de clientes
+        //console.log(clientes)
+        // Passo 2:Formatação do documento pdf
+        // p - portrait | l - landscape | mm e a4 (folha A4 (210x297mm))
         const doc = new jsPDF('p', 'mm', 'a4')
-        // inserir a data atual no relatorio
-        // Definir o tamanho da fonte
+        // Inserir imagem no documento pdf
+        // imagePath (caminho da imagem que será inserida no pdf)
+        // imageBase64 (uso da biblioteca fs par ler o arquivo no formato png)
+        const imagePath = path.join(__dirname, 'src', 'public', 'img', 'TEC_SANCHES.png')
+        const imageBase64 = fs.readFileSync(imagePath, { encoding: 'base64' })
+        doc.addImage(imageBase64, 'PNG', 5, 8) //(5mm, 8mm x,y)
+        // definir o tamanho da fonte (tamanho equivalente ao word)
+        doc.setFontSize(18)
+        // escrever um texto (título)
+        doc.text("Relatório de clientes", 14, 45)//x, y (mm)
+        // inserir a data atual no relatório
         const dataAtual = new Date().toLocaleDateString('pt-BR')
-        doc.setFontSize(16)
-        doc.text(`Data: ${dataAtual}`, 160, 10)
-        //  variavel de apoio na formatação
-        let y = 45
+        doc.setFontSize(12)
+        doc.text(`Data: ${dataAtual}`, 165, 10)
+        // variável de apoio na formatação
+        let y = 60
         doc.text("Nome", 14, y)
         doc.text("Telefone", 80, y)
         doc.text("E-mail", 130, y)
         y += 5
-        doc.setLineWidth(0.5)
-        doc.line(10, y, 200, y) //10 (inicio) ----- 200 (fim)
-        doc.text("Relatório de clientes", 14, 20) // x, y (mm)
+        // desenhar uma linha
+        doc.setLineWidth(0.5) // expessura da linha
+        doc.line(10, y, 200, y) // 10 (inicio) ---- 200 (fim)
+
+        // renderizar os clientes cadastrados no banco
+        y += 10 // espaçamento da linha
+        // percorrer o vetor clientes(obtido do banco) usando o laço forEach (equivale ao laço for)
+        clientes.forEach((c) => {
+            // adicionar outra página se a folha inteira for preenchida (estratégia é saber o tamnaho da folha)
+            // folha A4 y = 297mm
+            if (y > 280) {
+                doc.addPage()
+                y = 20 // resetar a variável y
+                // redesenhar o cabeçalho
+                doc.text("Nome", 14, y)
+                doc.text("Telefone", 80, y)
+                doc.text("E-mail", 130, y)
+                y += 5
+                doc.setLineWidth(0.5) 
+                doc.line(10, y, 200, y)
+                y += 10
+            }
+            doc.text(c.nomeCliente, 14, y),
+                doc.text(c.foneCliente, 80, y),
+                doc.text(c.emailCliente || "N/A", 130, y)
+            y += 10 //quebra de linha
+        })
+
+        // Adicionar numeração automática de páginas
+        const paginas = doc.internal.getNumberOfPages()
+        for (let i = 1; i <= paginas; i++) {
+            doc.setPage(i)
+            doc.setFontSize(10)
+            doc.text(`Página ${i} de ${paginas}`, 105, 290, {align: 'center'})
+        }
+
+        // Definir o caminho do arquivo temporário e nome do arquivo
         const tempDir = app.getPath('temp')
         const filePath = path.join(tempDir, 'clientes.pdf')
+        // salvar temporariamente o arquivo
         doc.save(filePath)
+        // abrir o arquivo no aplicativo padrão de leitura de pdf do computador do usuário
         shell.openPath(filePath)
     } catch (error) {
         console.log(error)
     }
 }
 
-
-
-// ========Fim do Relatorio de clientes
+// == Fim - relatório de clientes =============================
+// ============================================================
