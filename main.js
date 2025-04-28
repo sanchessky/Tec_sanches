ab33c3332e6f20a7b4f8c0e538f6fa2f7a3c6fb7
Thiago Sanches <thiagomlkgame@gmail.com>
2025-04-28 16:44:50 -0300

    Documentação Atualizada

--- main.js
+++ main.js
@@ -496,3 +496,22 @@ ipcMain.on('validate-search',()=>{
     })
 })
 //============================================================
+// ==Inicio CRUD DELETE ===============================================
+ipcMain.on('delete-client',async (event, id)=>{
+    console.log(id)
+    try {
+        // importante fazer a confirmação da exclusão
+        //usar a variavel let janela
+        const response = await dialog.showMessageBox(client,{
+            type:'warning',
+            title:"Atenção",
+            message: "Deseja realmente excluir esse cliente? \n Está ação não podera ser desfeita.",
+            buttons: ['Cancelar','Excluir']
+        })
+
+
+    } catch (error) {
+        console.log(error)
+    } 
+})
+// ==FIM CRUD DELETE ===============================================
